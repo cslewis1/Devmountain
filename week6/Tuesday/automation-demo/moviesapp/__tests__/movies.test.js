41e79536c3844c170dba21e9f21e9d7617768ef7
Charneaka Lewis <clewis20@ggc.edu>
2023-05-09 15:28:39 -0400

    editing demo

--- week6/Tuesday/automation-demo/moviesapp/__tests__/movies.test.js
+++ week6/Tuesday/automation-demo/moviesapp/__tests__/movies.test.js
@@ -1,6 +1,6 @@
 const { Builder, Browser, By, until, Key } = require("selenium-webdriver");
 
-let diver;
+let driver;
 
 beforeEach(async () => {
   driver = await new Builder().forBrowser(Browser.CHROME).build();
@@ -12,16 +12,15 @@ afterEach(async () => {
 
 describe("testing the movies app", () => {
   test("can add a movie", async() => {
-    //navigate to the webapp
-      await driver.get('http//localhost:3000')
-    //Find the input
-      await driver.findElement(By.css('input[name="movieTitle"]')).sendKeys('Encanto')
-    //Type in the movie
-    //Find the add button and click it
-      await driver.findElement(By.css('button[type="submit"]')).click()
-    //Wait until the movie apprears on the list
-      const addedMovie = await driver.wait(until.elementsLocated(By.css("movies-list li label")),1000)
-    //Check if the movie is on the list
-      expect(await addedMovie.getText()).toBe('Encanto')
+    //navigate to the web app
+    await driver.get('http://localhost:3000')
+    //Find the input and type in the movie
+    await driver.findElement(By.css('input[name="movieTitle"]')).sendKeys('Encanto')
+    //find the add button and click it
+    await driver.findElement(By.css('button[type="submit"]')).click()
+    //wait until the movie appears on the list
+    const addedMovie = await driver.wait(until.elementLocated(By.css("#movies-list li label")),1000)
+    //check if the movie is in the list
+    expect(await addedMovie.getText()).toBe('Encanto')
   });
 });
